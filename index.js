044747c07d8cd86ef363d253f5ef72da614361a4
ibrahimrahhal <ibrahim.rahhal3636@gmail.com>
2018-12-02 10:19:55 -0800

    added fullname route

--- index.js
+++ index.js
@@ -14,5 +14,34 @@ res.json(obj1);
 });
 
 
+
+
+
+
+/////////////////fullname route
+app.get("/fullname", (req,res)=>{
+var obj1= new Random();
+var obj2= new Random();
+var fullname ="";
+if(Math.random()>=0.49){
+fullname= obj1.randmalename[0]+" "+ obj2.randmalename[0]+" "+ obj2.randsurname[0];
+
+}else{
+fullname= obj1.randfmalename[0]+" "+ obj2.randmalename[0]+" "+ obj2.randsurname[0];
+
+
+}
+res.set("Access-Control-Allow-Origin", "*");
+res.send(fullname);
+
+});
+
+
+
+
+
+
+
+
 // app.listen(80,()=>{console.log("error");});
 app.listen(process.env.PORT || 8080,()=>{console.log("works");});
